dee62f2e1e2fb42251375ccf73bd1af22d8da377
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2018-07-18 18:08:27 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
+import { LocalStorageService } from './services/local-storage.service';
+import { AlertComponent } from './alert/alert.component';
+import { TickerComponent } from './ticker/ticker.component';
+import { StocksComponent } from './stocks/stocks.component';
+import { InvestmentsComponent } from './investments/investments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvestmentsComponent,
+    TickerComponent,
+    StocksComponent,
+    AlertComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    LocalStorageService,
+    CurrencyPipe
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
